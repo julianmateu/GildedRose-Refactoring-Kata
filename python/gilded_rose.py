a7666dd90eba8767ff476931e823c10df07b00b2
Julian Mateu <julianmateu@monzo.com>
2024-09-19 22:53:54 +0100

    extract loop

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -7,43 +7,46 @@ class GildedRose(object):
 
     def update_quality(self):
         for item in self.items:
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                ## Decreasing quality by 1 except brie, concert and sulfuras
-                if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
+            self.update_quality_for_item(item)
+    
+    def update_quality_for_item(self, item):
+        if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
+            ## Decreasing quality by 1 except brie, concert and sulfuras
+            if item.quality > 0:
+                if item.name != "Sulfuras, Hand of Ragnaros":
+                    item.quality = item.quality - 1
+        else:
+            ## Either brie or concert
+            if item.quality < 50:
+                item.quality = item.quality + 1
+                if item.name == "Backstage passes to a TAFKAL80ETC concert":
+                    ## concert will potentially enter both if conditions
+                    if item.sell_in < 11:
+                        if item.quality < 50:
+                            item.quality = item.quality + 1
+                    if item.sell_in < 6:
+                        if item.quality < 50:
+                            item.quality = item.quality + 1
+
+
+        ## This will always be executed
+        if item.name != "Sulfuras, Hand of Ragnaros":
+            item.sell_in = item.sell_in - 1
+        if item.sell_in < 0:
+            ## expired
+            if item.name != "Aged Brie":
+                if item.name != "Backstage passes to a TAFKAL80ETC concert":
+                    if item.quality > 0:
+                        if item.name != "Sulfuras, Hand of Ragnaros":
+                            ## if not brie, concert or sulfuras, decrease quality by 1 more
+                            item.quality = item.quality - 1
+                else:
+                    # concert
+                    item.quality = item.quality - item.quality ## set quality to 0
             else:
-                ## Either brie or concert
+                ## brie
                 if item.quality < 50:
                     item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        ## concert will potentially enter both if conditions
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-
-
-            ## This will always be executed
-            if item.name != "Sulfuras, Hand of Ragnaros":
-                item.sell_in = item.sell_in - 1
-            if item.sell_in < 0:
-                ## expired
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                ## if not brie, concert or sulfuras, decrease quality by 1 more
-                                item.quality = item.quality - 1
-                    else:
-                        # concert
-                        item.quality = item.quality - item.quality ## set quality to 0
-                else:
-                    ## brie
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
 
 
 class Item:

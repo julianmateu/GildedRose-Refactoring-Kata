921db4699005f46c985d96fd27da412e451c6261
Julian Mateu <julianmateu@monzo.com>
2024-09-20 17:04:12 +0100

    Refactor to OOP

--- go/gildedrose/gildedrose.go
+++ go/gildedrose/gildedrose.go
@@ -5,44 +5,84 @@ type Item struct {
 	SellIn, Quality int
 }
 
-func UpdateQuality(items []*Item) {
-	for _, item := range items {
-		updateQualityForItem(item)
+type ItemUpdater interface {
+	UpdateQuality(item *Item)
+	Matches(item *Item) bool
+}
+
+type AgedBrieUpdater struct{}
+
+func (u *AgedBrieUpdater) Matches(item *Item) bool {
+	return item.Name == "Aged Brie"
+}
+
+func (u *AgedBrieUpdater) UpdateQuality(item *Item) {
+	decreaseSellIn(item)
+	if expired(item) {
+		increaseQuality(item, 2)
+	} else {
+		increaseQuality(item, 1)
 	}
 }
 
-func updateQualityForItem(item *Item) {
-	if item.Name == "Sulfuras, Hand of Ragnaros" {
-		return
-	}
-	if item.Name == "Aged Brie" {
-		decreaseSellIn(item)
-		if expired(item) {
-			increaseQuality(item, 2)
-		} else {
-			increaseQuality(item, 1)
-		}
-		return
-	}
-	if item.Name == "Backstage passes to a TAFKAL80ETC concert" {
-		decreaseSellIn(item)
-		if expired(item) {
-			item.Quality = 0
-		} else if item.SellIn < 5 {
-			increaseQuality(item, 3)
-		} else if item.SellIn < 10 {
-			increaseQuality(item, 2)
-		} else {
-			increaseQuality(item, 1)
-		}
-		return
-	}
+type BackstagePassUpdater struct{}
 
+func (u *BackstagePassUpdater) Matches(item *Item) bool {
+	return item.Name == "Backstage passes to a TAFKAL80ETC concert"
+}
+
+func (u *BackstagePassUpdater) UpdateQuality(item *Item) {
+	decreaseSellIn(item)
+	if expired(item) {
+		item.Quality = 0
+	} else if item.SellIn < 5 {
+		increaseQuality(item, 3)
+	} else if item.SellIn < 10 {
+		increaseQuality(item, 2)
+	} else {
+		increaseQuality(item, 1)
+	}
+}
+
+type SulfurasUpdater struct{}
+
+func (u *SulfurasUpdater) Matches(item *Item) bool {
+	return item.Name == "Sulfuras, Hand of Ragnaros"
+}
+
+func (u *SulfurasUpdater) UpdateQuality(item *Item) {
+}
+
+type DefaultUpdater struct{}
+
+func (u *DefaultUpdater) Matches(item *Item) bool {
+	return true
+}
+
+func (u *DefaultUpdater) UpdateQuality(item *Item) {
 	decreaseSellIn(item)
 	if expired(item) {
 		increaseQuality(item, -2)
 	} else {
 		increaseQuality(item, -1)
+	}
+}
+
+var itemUpdaters = []ItemUpdater{
+	&SulfurasUpdater{},
+	&AgedBrieUpdater{},
+	&BackstagePassUpdater{},
+	&DefaultUpdater{},
+}
+
+func UpdateQuality(items []*Item) {
+	for _, item := range items {
+		for _, updater := range itemUpdaters {
+			if updater.Matches(item) {
+				updater.UpdateQuality(item)
+				break
+			}
+		}
 	}
 }
 

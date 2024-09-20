448786ad7a2512377d2bd6d43d1eddce768e2089
Julian Mateu <julianmateu@monzo.com>
2024-09-20 16:59:49 +0100

    Refactor logic

--- go/gildedrose/gildedrose.go
+++ go/gildedrose/gildedrose.go
@@ -9,7 +9,6 @@ func UpdateQuality(items []*Item) {
 	for _, item := range items {
 		updateQualityForItem(item)
 	}
-
 }
 
 func updateQualityForItem(item *Item) {
@@ -17,47 +16,45 @@ func updateQualityForItem(item *Item) {
 		return
 	}
 	if item.Name == "Aged Brie" {
-		if item.Quality < 50 {
-			item.Quality = item.Quality + 1
-		}
-		item.SellIn = item.SellIn - 1
-		if item.SellIn < 0 {
-			if item.Quality < 50 {
-				item.Quality = item.Quality + 1
-			}
+		decreaseSellIn(item)
+		if expired(item) {
+			increaseQuality(item, 2)
+		} else {
+			increaseQuality(item, 1)
 		}
 		return
 	}
 	if item.Name == "Backstage passes to a TAFKAL80ETC concert" {
-		if item.Quality < 50 {
-			item.Quality = item.Quality + 1
-			if item.SellIn < 11 {
-				if item.Quality < 50 {
-					item.Quality = item.Quality + 1
-				}
-			}
-			if item.SellIn < 6 {
-				if item.Quality < 50 {
-					item.Quality = item.Quality + 1
-				}
-			}
-		}
-		item.SellIn = item.SellIn - 1
-		if item.SellIn < 0 {
-			item.Quality = item.Quality - item.Quality
+		decreaseSellIn(item)
+		if expired(item) {
+			item.Quality = 0
+		} else if item.SellIn < 5 {
+			increaseQuality(item, 3)
+		} else if item.SellIn < 10 {
+			increaseQuality(item, 2)
+		} else {
+			increaseQuality(item, 1)
 		}
 		return
 	}
-	if item.Quality > 0 {
-		item.Quality = item.Quality - 1
-	}
 
+	decreaseSellIn(item)
+	if expired(item) {
+		increaseQuality(item, -2)
+	} else {
+		increaseQuality(item, -1)
+	}
+}
+
+func increaseQuality(item *Item, increase int) {
+	item.Quality = item.Quality + increase
+	item.Quality = max(0, min(item.Quality, 50))
+}
+
+func expired(item *Item) bool {
+	return item.SellIn < 0
+}
+
+func decreaseSellIn(item *Item) {
 	item.SellIn = item.SellIn - 1
-
-	if item.SellIn < 0 {
-		if item.Quality > 0 {
-			item.Quality = item.Quality - 1
-		}
-
-	}
 }
